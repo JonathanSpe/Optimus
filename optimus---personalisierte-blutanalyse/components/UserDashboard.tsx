b53b75d76c5d9871575789ea6fbefeedb0e579d8
JonathanSpe <118434515+JonathanSpe@users.noreply.github.com>
2026-03-20 11:20:24 +0100

    f

--- optimus---personalisierte-blutanalyse/components/UserDashboard.tsx
+++ optimus---personalisierte-blutanalyse/components/UserDashboard.tsx
@@ -5,7 +5,6 @@ import { MolecularOrbit } from './dashboard/MolecularOrbit';
 import { AnalysisPanel } from './dashboard/AnalysisPanel';
 import { SidebarHub } from './dashboard/SidebarHub';
 import { categoryData } from './dashboard/DashboardData';
-import AIChat from './AIChat';
 import { Activity, HeartPulse } from 'lucide-react';
 
 const SummarySidebar: React.FC = () => (
@@ -75,14 +74,6 @@ const UserDashboard: React.FC<{ onNavigate: (page: any) => void }> = ({ onNaviga
             </div>
             <div className="px-6 pb-6 pt-1">
               <AnalysisPanel activeData={activeData} onNavigate={onNavigate} />
-            </div>
-            <div className="border-t border-slate-200 bg-slate-50/50 px-6 py-4">
-              <p className="text-[10px] font-black uppercase tracking-[0.28em] text-slate-500">
-                AI Insight
-              </p>
-            </div>
-            <div className="px-6 pb-8 pt-2">
-              <AIChat />
             </div>
           </div>
 
